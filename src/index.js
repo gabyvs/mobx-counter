6156e4352c968958269ef3f3548654df23c3622b
Gaby Vazquez <gabs.vz@gmail.com>
2019-06-03 21:46:35 -0700

    Counter app - not wired

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Counter extends Component {
+
+  decrement = () => { this.count--; };
+
+  increment = () => { this.count++; };
+
+  render() {
+    return (
+      <div className="container">
+        Counter: {this.count}<br />
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.increment}>+</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
